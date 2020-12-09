f53af98de590b845e94a26eb6b7319823aea5a27
Ethan-23 <emanroberts@gmail.com>
2020-12-09 04:19:41 +0000

    Added number in {} for print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range (0, 10):
     for i in range (x + 1, 10):
-        print("{}{}".format(x, i), end="")
+        print("{0}{1}".format(x, i), end="")
         if x != 8:
             print(", ", end="")
         else:

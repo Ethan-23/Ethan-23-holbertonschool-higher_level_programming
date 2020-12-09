477b25d53fc8e6d1427953d30c8d5dd067848e1b
Ethan-23 <emanroberts@gmail.com>
2020-12-09 05:06:29 +0000

    First Commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(0, 100):
+        if x % 3 is 0:
+            print(Fizz)
+        elif x % 5 is 0:
+            print(Buzz)
+        else:
+            print("{} ".format(x), end="")
+    print()

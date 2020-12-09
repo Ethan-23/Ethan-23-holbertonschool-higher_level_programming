9a3935e1eba615a74c6918d587f0a4ce89d45d11
Ethan-23 <emanroberts@gmail.com>
2020-12-09 05:23:50 +0000

    Reverted and added string for strings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number > 9:
-    last = number % 10
-elif number < -9:
-    last = (number * -1) % 10
-    last = last * -1
+last = number % 10
+if number < 0:
+    last = 10 - last
+    last *= -1
+string = "Last digit of " + str(number) + " is " + str(last)
 if last > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last))
+    print("{} and is greater than 5".format(string))
 elif last == 0:
-    print("Last digit of {} is {} and is 0".format(number, last))
+    print("{} and is 0".format(string))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last))
+    print("{} and is less than 6 and not 0".format(string))

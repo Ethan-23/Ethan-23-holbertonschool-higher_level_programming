901208a7b5412c4b4381291366f0127b65935351
Ethan-23 <emanroberts@gmail.com>
2020-12-09 04:52:19 +0000

    Fixed for number under 10 and added return

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    val = number % 10
-    if number < 0:
-        val = 10 - val
-    print("{}".format(val), end="")
+    if number > 9:
+        number = number % 10
+        print(number, end="")
+        return(number)
+    elif number < -9:
+        number = number * -1 % 10
+        print(number, end="")
+        return(number)
+    else:
+        print(number, end="")
+        return(number)

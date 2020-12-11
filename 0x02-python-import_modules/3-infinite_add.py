7d38bce3c3d4a7601fcc89b592eba2b8b4459403
Ethan-23 <emanroberts@gmail.com>
2020-12-11 04:48:19 +0000

    removed 1 line to gain req check

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    length = len(argv)
     sum = 0
-    for i in range (1, length):
+    for i in range (1, len(argv)):
         sum += int(argv[i])
     print(sum)

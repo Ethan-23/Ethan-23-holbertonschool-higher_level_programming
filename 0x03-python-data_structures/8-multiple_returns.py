b66a769a91949de31ea9e82035869891ec5b8bf2
Ethan-23 <emanroberts@gmail.com>
2020-12-16 04:49:34 +0000

    Fixed spelling

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentance) == 0:
+    if len(sentence) == 0:
         tuple_empty = (0, None)
         return tuple_empty
-    tuple_1 = (len(sentance), sentance[0])
+    tuple_1 = (len(sentence), sentence[0])
     return tuple_1

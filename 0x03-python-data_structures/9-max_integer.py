7ad0bd1b21a78561ee42b9c77c3ac7562a49d9a4
Ethan-23 <emanroberts@gmail.com>
2020-12-16 04:54:54 +0000

    First Commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    z = my_list[0]
+    for x in my_list:
+        if x > my_list[x]
+        z = my_list[x]
+    return z

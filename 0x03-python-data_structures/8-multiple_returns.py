39cf9566c1db0a7a85774dea064df1deaffa5983
Ethan-23 <emanroberts@gmail.com>
2020-12-16 04:41:58 +0000

    First commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentance) == 0:
+        tuple_empty = (0, None)
+        return tuple_empty
+    tuple_1 = (len(sentance), sentance[0])
+    return tuple_1

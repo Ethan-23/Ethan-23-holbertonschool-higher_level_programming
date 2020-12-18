252da90fe89411549838a5eed1fdd08a3f374b4c
Ethan-23 <emanroberts@gmail.com>
2020-12-18 04:35:13 +0000

    Removed spaces before and after =

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
         return None
     if len(a_dictionary) == 0:
         return None
-    best = max(a_dictionary, key = a_dictionary.get)
+    best = max(a_dictionary, key=a_dictionary.get)
     return best

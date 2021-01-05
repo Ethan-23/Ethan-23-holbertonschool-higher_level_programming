ff8dae9ca0b4958f32f4288c9f7cf2e1759159d3
Ethan-23 <emanroberts@gmail.com>
2021-01-05 02:57:02 +0000

    First Commit

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    print("Inside result: ", end="")
+    try:
+        c = a/b
+    except:
+        c = "None"
+    finally:
+        print("{}".format(c))

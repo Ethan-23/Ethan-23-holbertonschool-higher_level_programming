3fd4084545b8d2b618a91b803a6f696419b040b5
Ethan-23 <emanroberts@gmail.com>
2021-01-05 03:10:30 +0000

    Fixed space in for loop

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for z in range (x):
+    for z in range(x):
             try:
                 print("{:d}".format(my_list[z]), end="")
                 count += 1

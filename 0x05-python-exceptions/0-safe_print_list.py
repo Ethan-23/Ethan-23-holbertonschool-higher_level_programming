bab23dac9b97eda8ce285d78b2bca4043ff3631d
Ethan-23 <emanroberts@gmail.com>
2021-01-05 02:09:21 +0000

    First Commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    h = 0
+    for y in my_list:
+        h += 1
+    print(h)
+    if x > h:
+        x = h
+    for z in my_list:
+        if z <= x:
+            try:
+                print(z, end="")
+            except:
+                print("An exception occurred")
+    print()
+    return x

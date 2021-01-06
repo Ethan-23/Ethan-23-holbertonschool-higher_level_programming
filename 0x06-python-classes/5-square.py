1d91f515a84c42dc6ce382b56aa495ccd46f9b5c
Ethan-23 <emanroberts@gmail.com>
2021-01-06 03:41:33 +0000

    Removed more spaces

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,9 +30,9 @@ class Square:
     def my_print(self):
         """Prints Square"""
         if self.__size == 0:
-            print ()
+            print()
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print ("#", end="")
-                print ()
+                    print("#", end="")
+                print()

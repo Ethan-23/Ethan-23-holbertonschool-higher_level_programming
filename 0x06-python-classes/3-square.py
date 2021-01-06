79f03d65f88684565e686af0cf610e2fbbd4ad11
Ethan-23 <emanroberts@gmail.com>
2021-01-06 03:16:20 +0000

    First Commit

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This will create a empty class that will do nothing!"""
+
+
+class Square:
+    """This is a empty class named Square with nothing in it!"""
+    def __init__(self, size=0):
+        """Sets size and error checking"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """area of the square"""
+        return self.__size ** 2

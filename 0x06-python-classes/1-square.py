2dc25117f7520812c73d2d024d733d9beade8ea7
Ethan-23 <emanroberts@gmail.com>
2021-01-06 02:31:20 +0000

    First Commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This will create a empty class that will do nothing!"""
+
+
+class Square:
+    """This is a empty class named Square with nothing in it!"""
+    def __init__(self, size):
+        """Sets size of the square"""
+        self.__size=size

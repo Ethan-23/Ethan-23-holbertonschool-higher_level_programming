b89eccceef9e52560738e9f454512c33514cfb96
Ethan-23 <emanroberts@gmail.com>
2021-01-06 02:17:45 +0000

    increased length

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This will create a empty class"""
-
+"""This will create a empty class that will do nothing!"""
 class Square:
-    """This is a empty class named Square"""
+    """This is a empty class named Square with nothing in it!"""
     pass

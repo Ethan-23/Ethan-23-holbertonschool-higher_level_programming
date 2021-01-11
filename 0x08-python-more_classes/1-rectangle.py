d4af37f636f93f623289ea2b235033566b9b2cee
Ethan-23 <emanroberts@gmail.com>
2021-01-11 23:50:06 +0000

    First COmmit

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This is an empty rectangle class"""
+
+
+class Rectangle:
+    """This is a class"""
+    def __init__(self, width=0, height=0):
+        """Sets sizes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """Property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """Property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__width = value

0fa3e219e5b7cedab84af49f7fe132df8711f79d
Ethan-23 <emanroberts@gmail.com>
2021-01-12 03:00:53 +0000

    First Commit

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This is an empty rectangle class"""
+
+
+class Rectangle:
+    """This is a class"""
+    def __init__(self, width=0, height=0):
+        """Sets sizes"""
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        """Property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Checks height for errors"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def width(self):
+        """Property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Checks widths for errors"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        """Area of rect"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Length of sides"""
+        return self.width * 2 + self.height * 2

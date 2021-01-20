fa43ebb38ea8a53d2997d77a39bb27e74d6ea17b
Ethan-23 <emanroberts@gmail.com>
2021-01-20 04:26:17 +0000

    Added self to area calc

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """Area"""
-        self.area = width * height
+        self.area = self.__width * self.__height
 
     def __str__:
         """string"""

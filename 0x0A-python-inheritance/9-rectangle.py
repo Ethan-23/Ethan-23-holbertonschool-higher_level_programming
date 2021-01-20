874dc858a34c8b4c2833e416ee8e72ade88ab5af
Ethan-23 <emanroberts@gmail.com>
2021-01-20 04:25:30 +0000

    First Commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Creates a class named BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class"""
+    def __init__(self, width, height):
+        """initialize"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Area"""
+        self.area = width * height
+
+    def __str__:
+        """string"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)

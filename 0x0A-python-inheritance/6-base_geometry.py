5fe67b3458a878553e89bb40c8cf81ce443b85ad
Ethan-23 <emanroberts@gmail.com>
2021-01-20 03:56:15 +0000

    First Commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Creates a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """Geometry Class"""
+    def area(self):
+        raise Exception("area() is not implemented")

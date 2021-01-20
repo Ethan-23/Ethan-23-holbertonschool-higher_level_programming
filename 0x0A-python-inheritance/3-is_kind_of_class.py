6035e8bd8c23350132bcc9d233259ef4cc3e9171
Ethan-23 <emanroberts@gmail.com>
2021-01-20 03:23:37 +0000

    Changes for pep8

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""object is an instance of, or if the object is an instance of a class that inherited from, the specified class"""
+"""object is an instance of, or if the object is an instance of a class"""
 
 
 def is_kind_of_class(obj, a_class):

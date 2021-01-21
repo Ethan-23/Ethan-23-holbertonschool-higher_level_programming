bd249df74f2c071248ea8f7f938b32e1d423151e
Ethan-23 <emanroberts@gmail.com>
2021-01-21 01:51:57 +0000

    First Commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Student Class"""
+
+
+class Student:
+    """Class"""
+    def __init__(self, first_name, last_name, age):
+        """Init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """json"""
+        if attrs is None or type(attrs) is not list:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for x in attrs:
+                if type(x) is str:
+                    try:
+                        new_dict[x] = self.__dict__[x]
+                    except:
+                        pass
+            return new_dict

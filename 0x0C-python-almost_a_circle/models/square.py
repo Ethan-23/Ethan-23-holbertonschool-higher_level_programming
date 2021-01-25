8a91ebc3a872bb8ba50c60c69816e57a41d64000
Ethan-23 <emanroberts@gmail.com>
2021-01-25 03:11:32 +0000

    Updated with update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,17 @@ class Square(Rectangle):
     def __str__(self):
         square_str_print = "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
         return square_str_print
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)

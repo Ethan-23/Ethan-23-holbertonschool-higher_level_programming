44e1442e7a55f4e4412b7e93e32d00a2b914f9b5
Ethan-23 <emanroberts@gmail.com>
2021-01-27 05:56:23 +0000

    Test Change

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,7 @@ class Rectangle(Base):
     print_symbol = "#"
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Init'''
-        super(Base, self).__init__(id)
+        super(Rectangle, self).__init__(id)
         self.width = width
         self.height = height
         self.x = x

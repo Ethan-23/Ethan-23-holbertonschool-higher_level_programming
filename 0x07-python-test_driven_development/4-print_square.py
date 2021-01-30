ca9259a3789e63e86120fb89a9b73c89f9faaa9a
Ethan-23 <emanroberts@gmail.com>
2021-01-30 06:27:58 +0000

    Added task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,6 +7,10 @@ I am making up some words to get a better score I think
 
 def print_square(size):
     """Prints a square with a given size"""
+    if type(size) is not int:
+        TypeError("size must be an integer")
+    if size < 0:
+        ValueError("size must be >= 0")
     for i in range(size):
         for j in range(size):
             print("#", end="")

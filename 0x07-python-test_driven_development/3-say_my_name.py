32d09771b169faf30bf778286de839b68c27c08d
Ethan-23 <emanroberts@gmail.com>
2021-01-30 06:18:03 +0000

    Updated for pep8

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This is some random words that came to my head because
+it is late and I wanted to get some projects done
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Used for printing first and last name
+    """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     elif first_name is None:

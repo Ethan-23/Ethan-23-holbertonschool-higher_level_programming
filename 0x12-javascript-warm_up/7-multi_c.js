116aadf3aa382eadb34e76db3b3348503277454a
Ethan-23 <emanroberts@gmail.com>
2021-03-23 03:15:39 +0000

    added space

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv[2]) {
-for (let j = 0; j < parseInt(process.argv[2]); j++){
+for (let j = 0; j < parseInt(process.argv[2]); j++) {
   console.log('C is fun');
 }
 } else {

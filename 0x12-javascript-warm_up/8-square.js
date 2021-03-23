01e12435f7776bbf16d169e6935867f8701ca2a1
Ethan-23 <emanroberts@gmail.com>
2021-03-23 03:20:34 +0000

    added task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv[2]) {
+for (let j = 0; j < parseInt(process.argv[2]); j++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+}
+} else {
+  console.log('Missing number of occurrences');
+}

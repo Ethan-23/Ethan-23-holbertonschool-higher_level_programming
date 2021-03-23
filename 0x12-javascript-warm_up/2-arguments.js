bfb260693af4ee6ce25909b997235ac733ebe7dc
Ethan-23 <emanroberts@gmail.com>
2021-03-23 02:53:47 +0000

    trying again

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length > 2) {
-    if (process.argv.length <= 3) {
+    if (process.argv.length < 4) {
 	console.log('Argument found');
     } else {
 	console.log('Arguments found');

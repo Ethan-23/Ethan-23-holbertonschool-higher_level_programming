07c1ba83a15257a0d72703747ac7ca5b4754a1d2
Ethan-23 <emanroberts@gmail.com>
2021-03-27 17:52:46 +0000

    added task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Things
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    states = MySQLdb.connect(user=sys.argv[1],
+                             password=sys.argv[2],
+                             host='localhost',
+                             database=sys.argv[3],
+                             port=3306)
+    statesc = states.cursor()
+    statesc.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = statesc.fetchall()
+    for i in rows:
+        if sys.argv[4] == i[1]:
+            print(i)
+    statesc.close()
+    states.close()

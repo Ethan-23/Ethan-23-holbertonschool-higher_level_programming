7ab4aab80e2f5d974722c79ee4243b56e776bc33
Ethan-23 <emanroberts@gmail.com>
2021-05-05 03:41:19 +0000

    repush 2

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const swapi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get({ url: swapi}, function (error, response, body) {
+request.get({ url: swapi }, function (error, response, body) {
   if (error) console.error(error);
   const chars = JSON.parse(body).characters;
   for (const charapi of chars) {

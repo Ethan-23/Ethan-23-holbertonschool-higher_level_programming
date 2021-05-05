dfdaae3c1741bfd825ba47293f68b6e86ce484a6
Ethan-23 <emanroberts@gmail.com>
2021-05-05 03:34:31 +0000

    added task 7

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const swapi = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get({ url: swapi}, function (error, response, body) {
+  if (error) console.error(error);
+  const chars = JSON.parse(body).characters;
+  for (const charapi of chars) {
+    request.get({ url: charapi }, function (error, response, body) {
+      if (error) console.error(error);
+      console.log(JSON.parse(body).name)
+    });
+  }
+});

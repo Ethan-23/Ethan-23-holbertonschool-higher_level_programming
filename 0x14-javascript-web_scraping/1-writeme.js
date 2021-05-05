11f312b2ff9065d457505060200029143a829ffb
Ethan-23 <emanroberts@gmail.com>
2021-05-05 02:35:13 +0000

    added task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs')
+let data = process.argv[3]
+
+fs.writeFile(process.argv[2], data, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});

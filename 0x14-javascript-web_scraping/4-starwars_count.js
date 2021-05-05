c93288a11fb822af46b7b52b8db2603932780e81
Ethan-23 <emanroberts@gmail.com>
2021-05-05 03:08:03 +0000

    added task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  let count = 0;
+  const titlelist = JSON.parse(body).results;
+  for (let i = 0; i < titlelist.length; i++) {
+    for (let j = 0; j < titlelist[i].characters.length; j++) {
+      if (titlelist[i].characters[j].split('/')[5] === "18") {
+	count++;
+	break;
+      }
+    }
+  }
+  console.log(count);
+});
